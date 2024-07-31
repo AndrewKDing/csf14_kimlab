a542f36596a49a2ad506e2d85e1cfc99117f0a71
Andrew Ding <dingaa@bc.edu>
2024-07-31 10:02:05 -0400

    the date extraction function works, next i need to finish the function to calculate dpi

--- scripts/filefinder.R
+++ scripts/filefinder.R
@@ -29,16 +29,75 @@ dir_finder <- function(pathname, cohort) {
   return(cohort_list)
 }
 
-# fthis should be used in the main script
+# this should be used in the main script
 lapply(cohort_list, list.files)
 
 # filename_extract pulls data from filename. It looks for date, an 8 digit string,
 # then looks for animal id, a 5 digit string.
-# returns a tibble with [animal id, date]
-filename_extract <- function(filenames) {
+# returns a tibble with [filename, animal id, date]
+# assumes the MMDDYYY format used by Andradi
+filename_extract <- function(filenames, format = NULL) {
+  if (is.null(format)){
+    format <-  "%m%d%Y"
+  } else {
+    format <-  format
+  }
+  
   date <- str_extract(filenames[[1]], "\\d{8}")
-  animal_id <- str_extract(filenames[[1]], "\\d{5}")
+  animal_id <- as.factor(str_extract(filenames[[1]], "\\d{5}"))
 
-  df <- tibble(animal_id, date)
+  df <- tibble(filenames[[1]], animal_id, date)
+  
+  #convert the date into an actual date
+  df <- df %>%
+    mutate(date = as.Date(date, "%m%d%Y"))
 }
 
+#calculates dpi, takes the cohort table and a tibble that has animal ids
+#datevar and idvar are the names of the variables in the id and lookup dataframes,
+# respectively. 
+
+#Specifies names of variables for idvar and datevar, expects variable names
+# to be the same
+calculate_dpi <- function(ids_df, 
+                          lookup_df,
+                          idvar,
+                          datevar,
+                          ){
+  
+  #Only infection date is required from the lookup
+  lookup_df <- lookup_df %>%
+    select(idvar, datevar)
+  
+  #Joining the dataframes
+  dpi_df <- left_join(ids_df, lookup_df, by=c(idvar, datevar))
+}
+
+
+
+#really should be done in a main script and not in my function calls
+#Uses andradi's as a default
+if (is.null(datevar)){
+  datevar <- "date"
+} else {
+  datevar <- datevar
+}
+
+if (is.null(datevar2)){
+  datevar2 <- "Date_of_infection"
+} else {
+  datevar2 <- datevar2
+}
+
+#Uses andrad's id var convention as a default
+if (is.null(idvar)) {
+  idvar <- "animal_id"
+} else {
+  idvar <- idvar    
+}
+
+if (is.null(idvar2)) {
+  idvar2 <- "Animal_ID"
+} else{
+  idvar2 <- idvar
+}
